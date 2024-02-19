abafe50b5b874dcc87d21fcce3a5e039dc470267
leezabajra <leeza.bajracharya@infodevelopers.local>
2024-02-19 16:32:33 +0545

    changes in aplication of by birth and according to birth

--- cypress/e2e/acctobirth.cy.js
+++ cypress/e2e/acctobirth.cy.js
@@ -1,12 +1,11 @@
-    
+import acctobirth from '../fixtures/acctobirth.json';  
     describe('Login Test', () => {
-        beforeEach(() => {
-          cy.visit('https://ccimsinternal.infodev.com.np/');
-        });
+     acctobirth.forEach((user) => {
+
           
         it('login', () => {
           
-          
+          cy.visit('https://ccimsinternal.infodev.com.np/');
           cy.get('.form-control').eq(0).type('mohan');
           cy.get('.form-control').eq(1).type('Test@123');
           cy.get('.btn').click();
@@ -14,14 +13,14 @@
           cy.get(':nth-child(5) > .mastermenu-link').click();
          
           cy.get(':nth-child(6) > .submenu-link').click();
-          cy.get('#firstNameNp').type('सुधा');
-          cy.get('#lastNameNp').type('कार्की');
-          cy.get('#firstName').type('sudha');
-          cy.get('#lastName').type('karki');
-          cy.get('#gender').type('f');
+          cy.get('#firstNameNp').type(firstNameNp);
+          cy.get('#lastNameNp').type(lastNameNp);
+          cy.get('#firstName').type(firstName);
+          cy.get('#lastName').type(lastName);
+          cy.get('#gender').type(gender);
 
-          cy.get('#birthRegistrationNo').type('665236');
-          cy.get('.col-4 > .form-group > .formValidationDiv > app-date-picker > .justify-content-between > .input-icon-BS > .form-control').type('20621111{enter}');
+          cy.get('#birthRegistrationNo').type(birthRegistrationNo);
+          cy.get('.col-4 > .form-group > .formValidationDiv > app-date-picker > .justify-content-between > .input-icon-BS > .form-control').type(DOB+'{enter}');
           
           cy.get('#district_address0').click();
           cy.get(':nth-child(4) > tb-search-field > .form-control-icon > .header-filter').type('bhaktapur');
@@ -31,34 +30,34 @@
           cy.get('tbody > :nth-child(2) > :nth-child(3)').click();
           cy.wait(2000);
           cy.get(':nth-child(5) > :nth-child(2) > .col-2 > .form-group > .select2 > .selection > .select2-selection').click();
-          cy.get('.select2-search__field').type('5{enter}');
+          cy.get('.select2-search__field').type(wardno +'{enter}');
 
           cy.get('#sameAsPermanentAddress_address1').click();
 
-          cy.get('#firstNameNp_0').type('नरेन');
-          cy.get('#firstName_0').type('naren');
+          cy.get('#firstNameNp_0').type(firstNameNp_0);
+          cy.get('#firstName_0').type(firstName_0);
           cy.wait(2000);
-          cy.get('#citizenshipNo_0').type('86242');
+          cy.get('#citizenshipNo_0').type(citizenshipNo_0);
           cy.get('#citizenshipType_0').select('वंशज');
           cy.get('#localBody_family0').click();
           cy.get('tbody > :nth-child(2) > :nth-child(2)').click();
           cy.wait(2000);
           cy.get('.application-card-form > app-address-module > :nth-child(2) > .col-2 > .form-group > .select2 > .selection > .select2-selection').click();
-          cy.get('.select2-search__field').type('5{enter}');
+          cy.get('.select2-search__field').type(wardno +'{enter}');
           
 
           cy.get('.nav > :nth-child(2)').click();
-          cy.get('#firstNameNp_1').type('अमृता');
-          cy.get('#firstName_1').type('amrita');
-          cy.get('#citizenshipNo_1').type('26546');
+          cy.get('#firstNameNp_1').type(firstNameNp_1);
+          cy.get('#firstName_1').type(firstName_1);
+          cy.get('#citizenshipNo_1').type(citizenshipNo_1);
           cy.get('#citizenshipType_1').select('वंशज');
           cy.get('#localBody_family1').click();
           cy.get('tbody > :nth-child(2) > :nth-child(2)').click();
           cy.get('.application-card-form > app-address-module > :nth-child(2) > .col-2 > .form-group > .select2 > .selection > .select2-selection').click();
-          cy.get('.select2-search__field').type('5{enter}');
+          cy.get('.select2-search__field').type(wardno+'{enter}');
 
-          cy.get('#recommendFirstNameNp').type('श्याम');
-          cy.get('#recommendLastNameNp').type('लामा');
+          cy.get('#recommendFirstNameNp').type(recommendFirstNameNp);
+          cy.get('#recommendLastNameNp').type(recommendLastNameNp);
           cy.get('.mb-2.mt-1 > .col-4 > .form-group > .formValidationDiv > .input-group > #officeName').click();
           cy.get('tbody > :nth-child(1) > :nth-child(2)').click();
 
@@ -96,4 +95,4 @@
 
         });
       });
-      
+    });
